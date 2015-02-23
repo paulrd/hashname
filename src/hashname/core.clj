71eb7f4dd4888987fe2c981591fa376afdd38dd9
Paul Dumais <paul@unstate.ca>
2015-02-22 23:05:01 -0700

    hashname encoding partially working

--- src/hashname/core.clj
+++ src/hashname/core.clj
@@ -1,6 +1,25 @@
-(ns hashname.core)
+(ns hashname.core
+  (:import org.apache.commons.codec.binary.Base32
+           org.apache.commons.codec.digest.DigestUtils))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn rf [rollup [k v]]
+  (let [ru (try (-> k (Byte/parseByte 16) vector byte-array (#(concat rollup %))
+                    byte-array DigestUtils/sha256)
+                (catch NumberFormatException e 
+                  (throw (Throwable. "cipher key must be a valid hex string"))))]
+    (->> v .toUpperCase (.decode (Base32.))
+         DigestUtils/sha256 (concat ru) byte-array DigestUtils/sha256)))
+
+(defn from-keys
+  "generate hashname from keys json, vals are either base32 keys or key binary
+  Buffer's"
+  [keys-map]
+  (when (empty? keys-map) (throw (Throwable. "keys-map should not be empty")))
+  (->> keys-map sort (reduce rf (byte-array [])) (.encodeToString (Base32.)) 
+       (#(.split % "=")) (String/join "") .toLowerCase))
+
+
+
+
+
+
--- test/hashname/core_test.clj
+++ test/hashname/core_test.clj
@@ -2,6 +2,25 @@
   (:require [clojure.test :refer :all]
             [hashname.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest keys-test
+  (testing "should generate from two keys"
+    (let [ks {"3a" "eg3fxjnjkz763cjfnhyabeftyf75m2s4gll3gvmuacegax5h6nia"
+              "1a" "an7lbl5e6vk4ql6nblznjicn5rmf3lmzlm"}]
+      (is (= (from-keys ks) 
+             "27ywx5e5ylzxfzxrhptowvwntqrd3jhksyxrfkzi6jfn64d3lwxa")))))
+
+(deftest one-key
+  (testing "should generate from one key"
+    (let [k {"1a" "vgjz3yjb6cevxjomdleilmzasbj6lcc7"}]
+      (is (= (from-keys k)
+             "echmb6eke2f6z2mqdwifrt6i6hkkfua7hiisgrms6pwttd6jubiq")))))
+
+(deftest no-keys
+  (testing "should fail with no keys"
+    (is (thrown-with-msg? Throwable #"should not be empty" (from-keys {})))))
+
+(deftest bad-key
+  (testing "should fail with bad key"
+    (let [k {"bad" "8jze4merv08q6med3u21y460fjdcphkyuc858538mh48zu8az39t1vxdg9tadzun"}]
+      (is (thrown-with-msg? Throwable #"must be a valid hex string" 
+                            (from-keys k))))))
